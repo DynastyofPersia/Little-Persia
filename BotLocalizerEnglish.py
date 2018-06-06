6f50b8284d1c4ecddf3a5fc545037d12224ff653
DynastyofPersia <39982400+DynastyofPersia@users.noreply.github.com>
2018-06-06 02:58:44 -0400

    Create BotLocalizerEnglish.py

---
+++ BotLocalizerEnglish.py
@@ -0,0 +1,41 @@
+# Filename: BotLocalizerEnglish.py
+# Author: mfwass
+# Date: January 8th, 2017
+#
+# The Legend of Pirates Online Software
+# Copyright (c) The Legend of Pirates Online. All rights reserved.
+#
+# All use of this software is subject to the terms of the revised BSD
+# license.  You should have received a copy of this license along
+# with this source code in a file named "LICENSE."
+
+"""
+The class BotLocalizerEnglish is used when providing responses
+in Discord channels where English is the spoken language.
+All strings in this class will be in English only.
+"""
+
+OUT_OF_DATE = "This bot is out of date. Please update by visiting https://github.com/TheLegendofPiratesOnline/discord-bot"
+
+FLEET_ITEM_INFO = '''
+%s:
+    - Type: %s
+    - State: %s
+    - Ships Remaining: %s
+'''
+
+INVASION_ITEM_INFO = '''
+%s:
+    - Location: %s
+    - State: %s
+    - Phase: %s
+    - Num Players: %s
+'''
+
+SYSTEM_STATUS_INFO = '''
+Overall Status: **%s**
+%s
+Reported Outages: %s
+'''
+
+OCEANS_TOTAL = '\nTotal: **%s**'
